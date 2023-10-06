cea5bc11d110f10e192dbd88a870086d11769f27
unknown <mlokman286@gmail.com>
2023-10-06 11:52:02 +0600

    Somthing is  wrong

--- minblog/blog/views.py
+++ minblog/blog/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import LoginForm, SignUpForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 # Home
@@ -10,3 +14,39 @@ def about(request):
 # Contact
 def contact(request):
     return render(request,'blog/contact.html')
+# DashBoard
+def dashboard(request):
+    return render(request,'blog/dahboard.html')
+# Signup
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,'Congratulations !! You have become a author ✔✔👍')
+            form.save()
+    else:
+        form = SignUpForm()
+    return render(request,'blog/signup.html',{'form':form})
+# Login 
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = LoginForm(request=request,data= request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate( username = uname,password = upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Logged in Successfully')
+                    return HttpResponseRedirect('/dashboard/')
+                else:
+                    messages.error(request,'Invalid Username or Password')
+        else:
+            form = LoginForm()
+        return render(request,'blog/login.html',{'form':form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
+# logout
+def user_logout(request):
+    return HttpResponseRedirect('/')
